9765ace004d5db400d983a8d3d50b2cb1eb15eac
gajamedia <72379650+gajamedia@users.noreply.github.com>
2025-01-05 08:21:11 +0700

    Add files via upload

---
+++ dbsales.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Community v12.4.3 (32 bit)
+MySQL - 8.0.37 : Database - dbsales
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbsales` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `dbsales`;
+
+/*Table structure for table `tb_bahan` */
+
+DROP TABLE IF EXISTS `tb_bahan`;
+
+CREATE TABLE `tb_bahan` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `ItemCode` varchar(15) DEFAULT NULL,
+  `ItemName` varchar(255) DEFAULT NULL,
+  `IdJenis` int DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `tb_bahan` */
+
+/*Table structure for table `tb_jenisbahan` */
+
+DROP TABLE IF EXISTS `tb_jenisbahan`;
+
+CREATE TABLE `tb_jenisbahan` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `NamaJenis` varchar(255) DEFAULT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `created_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` varchar(100) DEFAULT NULL,
+  `updated_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_deleted` int DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `tb_jenisbahan` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
