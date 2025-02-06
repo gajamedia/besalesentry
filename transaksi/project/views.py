81f6af4c78ad4e8ec330543c9afe0cd6420a43dd
gajamedia <72379650+gajamedia@users.noreply.github.com>
2025-02-06 22:16:16 +0700

    Update API project header dan project detil

--- transaksi/project/views.py
+++ transaksi/project/views.py
@@ -19,7 +19,6 @@ class PagePagination(PageNumberPagination):
 class ProjectHeaderViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]  # API hanya bisa diakses oleh user yang login
 
-
     @swagger_auto_schema(
         manual_parameters=[
             openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
@@ -145,6 +144,75 @@ class ProjectHeaderViewSet(viewsets.ViewSet):
         else:
             return Response({"error": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['no_project', 'tgl_project', 'nama_customer', 'status_project'],
+            properties={
+                'no_project': openapi.Schema(type=openapi.TYPE_STRING, description='Nomor Project'),
+                'tgl_project': openapi.Schema(type=openapi.TYPE_STRING, format="date-time", description='Tanggal Project (YYYY-MM-DD HH:MM:SS)'),
+                'ket_project': openapi.Schema(type=openapi.TYPE_STRING, description='Keterangan Project'),
+                'nama_customer': openapi.Schema(type=openapi.TYPE_STRING, description='Nama Customer'),
+                'addr_customer': openapi.Schema(type=openapi.TYPE_STRING, description='Alamat Customer'),
+                'contact_customer': openapi.Schema(type=openapi.TYPE_STRING, description='Kontak Customer'),
+                'status_project': openapi.Schema(type=openapi.TYPE_STRING, maxLength=1, description='Status Project (1 karakter)'),
+            }
+        ),
+        responses={200: "Updated"},
+    )
+    def update(self, request, pk=None):
+        """ Mengupdate data berdasarkan ID """
+        no_project = request.data.get("no_project")
+        tgl_project = request.data.get("tgl_project")
+        ket_project = request.data.get("ket_project", "")
+        nama_customer = request.data.get("nama_customer")
+        addr_customer = request.data.get("addr_customer", "")
+        contact_customer = request.data.get("contact_customer", "")
+        status_project = request.data.get("status_project")
+        username = request.user.username  # Username pengguna yang login
+        updated_date = now()
+
+        # Validasi input
+        if not (no_project and tgl_project and nama_customer and status_project):
+            return Response({"error": "Required fields are missing"}, status=status.HTTP_400_BAD_REQUEST)
+
+        with connections['mysql'].cursor() as cursor:
+            cursor.execute("""
+                    UPDATE tb_project_header SET no_project=%s, tgl_project=%s, ket_project=%s, nama_customer=%s, 
+                        addr_customer=%s, contact_customer=%s, status_project=%s, updated_by=%s, updated_date=%s
+                    WHERE id=%s
+                """,
+                [no_project, tgl_project, ket_project, nama_customer, addr_customer, contact_customer,
+                    status_project, username, updated_date]
+            )
+
+        return Response({"id": pk, "no_project": no_project, "tgl_project": tgl_project, 
+                         "ket_project": ket_project, "nama_customer": nama_customer, "addr_customer": addr_customer,  
+                         "contact_customer": contact_customer, "status_project": status_project,
+                         "updated_by": username, "updated_date": updated_date}, status=status.HTTP_200_OK)
+
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        responses={204: "Deleted"},
+    )
+    def destroy(self, request, pk=None):
+        """ Soft Delete: Menandai data sebagai is_deleted = 1 """
+        username = request.user.username  # Username pengguna yang login
+        updated_date = now()
+
+        with connections['mysql'].cursor() as cursor:
+            cursor.execute(
+                "UPDATE tb_project_header SET is_deleted=1, updated_by=%s, updated_date=%s WHERE id=%s",
+                [username, updated_date, pk]
+            )
+
+        return Response({"message": "Soft deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
     @swagger_auto_schema(
         manual_parameters=[
@@ -235,7 +303,7 @@ class ProjectHeaderViewSet(viewsets.ViewSet):
             "results": data
         }, status=status.HTTP_200_OK)
 
-
+##################################################################################################################################################################
 # Detail Project 
 class ProjectDetilViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]  # API hanya bisa diakses oleh user yang login
@@ -352,3 +420,216 @@ class ProjectDetilViewSet(viewsets.ViewSet):
 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('id', openapi.IN_PATH, description="ID Project Detil", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        responses={200: "Success"},
+    )
+    def retrieve(self, request, pk=None):
+        """ Mendapatkan detail data berdasarkan ID """
+        with connections['mysql'].cursor() as cursor:
+            cursor.execute("SELECT id, id_project_header, lebar_bahan, lantai, ruangan, bed, tipe, " + 
+                           "uk_room_l, uk_room_p, uk_room_t, stik, elevasi, tinggi_vitrase, tinggi_lipatan, nilai_pembagi, " + 
+                           "created_by, created_date, updated_by, updated_date, is_deleted " + 
+                           "FROM tb_project_detil WHERE id = %s", [pk])
+            row = cursor.fetchone()
+
+        if row:
+            return Response(
+                {
+                    "id": row[0],
+                    "id_project_header": row[1],
+                    "lebar_bahan": row[2],
+                    "lantai": row[3],
+                    "ruangan": row[4],
+                    "bed": row[5],
+                    "tipe": row[6],
+                    "uk_room_l": row[7],
+                    "uk_room_p": row[8],
+                    "uk_room_t": row[9],
+                    "stik": row[10],
+                    "elevasi": row[11],
+                    "tinggi_vitrase": row[12],
+                    "tinggi_lipatan": row[11],
+                    "nilai_pembagi": row[12],
+                    "created_by": row[13],
+                    "created_date": row[14],
+                    "updated_by": row[15],
+                    "updated_date": row[16],
+                    "is_deleted": row[17],
+                },
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response({"error": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['id_project_header'],
+            properties={
+                'id_project_header': openapi.Schema(type=openapi.TYPE_STRING, description='id Project header'),
+                'lebar_bahan': openapi.Schema(type=openapi.TYPE_STRING, description='Lebar Bahan'),
+                'lantai': openapi.Schema(type=openapi.TYPE_STRING, description='Lantai'),
+                'ruangan': openapi.Schema(type=openapi.TYPE_STRING, description='Ruangan'),
+                'bed': openapi.Schema(type=openapi.TYPE_STRING, description='No.Bed'),
+                'tipe': openapi.Schema(type=openapi.TYPE_STRING, description='Tipe'),
+                'uk_room_l': openapi.Schema(type=openapi.TYPE_STRING, description='Lebar Ruangan'),
+                'uk_room_p': openapi.Schema(type=openapi.TYPE_STRING, description='Panjang Ruangan'),
+                'uk_room_t': openapi.Schema(type=openapi.TYPE_STRING, description='Tinggi Ruangan'),
+                'stik': openapi.Schema(type=openapi.TYPE_STRING, description='Stik'),
+                'elevasi': openapi.Schema(type=openapi.TYPE_STRING, description='Elevasi'),
+                'tinggi_vitrase': openapi.Schema(type=openapi.TYPE_STRING, description='Tinggi Vitrase'),
+                'tinggi_lipatan': openapi.Schema(type=openapi.TYPE_STRING, description='Tinggi Lipatan'),
+                'nilai_pembagi': openapi.Schema(type=openapi.TYPE_STRING, description='Nilai Pembagi'),
+            }
+        ),
+        responses={200: "Updated"},
+    )
+    def update(self, request, pk=None):
+        """ Mengupdate data berdasarkan ID """
+        id_project_header = request.data.get("id_project_header")
+        lebar_bahan = request.data.get("lebar_bahan")
+        lantai = request.data.get("lantai", "")
+        ruangan = request.data.get("ruangan")
+        bed = request.data.get("bed", "")
+        tipe = request.data.get("tipe", "")
+        uk_room_l = request.data.get("uk_room_l")
+        uk_room_p = request.data.get("uk_room_p")
+        uk_room_t = request.data.get("uk_room_t")
+        stik = request.data.get("stik", "")
+        elevasi = request.data.get("elevasi")
+        tinggi_vitrase = request.data.get("tinggi_vitrase", "")
+        tinggi_lipatan = request.data.get("tinggi_lipatan", "")
+        nilai_pembagi = request.data.get("nilai_pembagi")
+        username = request.user.username  # Username pengguna yang login
+        updated_date = now()
+
+        if not id_project_header:
+            return Response({"error": "Item Code is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        with connections['mysql'].cursor() as cursor:
+            cursor.execute("""
+                    UPDATE tb_project_detil SET id_project_header=%s, lebar_bahan=%s, lantai=%s, ruangan=%s, bed=%s, tipe=%s, 
+                        uk_room_l=%s, uk_room_p=%s, uk_room_t=%s, stik=%s, elevasi=%s, tinggi_vitrase=%s, tinggi_lipatan=%s, 
+                        nilai_pembagi=%s, updated_by=%s, updated_date=%s
+                    WHERE id=%s
+                """,
+                [id_project_header, lebar_bahan, lantai, ruangan, bed, tipe, uk_room_l, uk_room_p, uk_room_t, 
+                 stik, elevasi, tinggi_vitrase, tinggi_lipatan, nilai_pembagi, username, updated_date, pk]
+            )
+
+        return Response({"id": pk, "id_project_header": id_project_header, "lebar_bahan": lebar_bahan, 
+                         "lantai": lantai, "ruangan": ruangan, "bed": bed, "tipe": tipe, 
+                         "stik": stik, "elevasi": elevasi, "tinggi_vitrase": tinggi_vitrase, "tinggi_lipatan": tinggi_lipatan,
+                         "nilai_pembagi": nilai_pembagi, "updated_by": username, "updated_date": updated_date}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        responses={204: "Deleted"},
+    )
+    def destroy(self, request, pk=None):
+        """ Soft Delete: Menandai data sebagai is_deleted = 1 """
+        username = request.user.username  # Username pengguna yang login
+        updated_date = now()
+
+        with connections['mysql'].cursor() as cursor:
+            cursor.execute(
+                "UPDATE tb_project_detil SET is_deleted=1, updated_by=%s, updated_date=%s WHERE id=%s",
+                [username, updated_date, pk]
+            )
+
+        return Response({"message": "Soft deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('search', openapi.IN_QUERY, description="Search query", type=openapi.TYPE_STRING),
+            openapi.Parameter('page', openapi.IN_QUERY, description="Page number", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('page_size', openapi.IN_QUERY, description="Page size", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer "),
+        ],
+        responses={200: "Success"}
+    )
+    @action(detail=False, methods=["get"], url_path="search")
+    def search(self, request):
+        """
+        Search data tb_project_detil berdasarkan beberapa field dengan pagination
+        """
+        search_query = request.GET.get("search", "")
+        page = int(request.GET.get("page", 1))
+        page_size = int(request.GET.get("page_size", 10))
+
+        with connections["mysql"].cursor() as cursor:
+            # Hitung total hasil
+            cursor.execute("""
+                SELECT COUNT(*) 
+                FROM tb_project_detil
+                WHERE is_deleted = 0 AND (
+                    id_project_header LIKE %s OR 
+                    lebar_bahan LIKE %s OR 
+                    lantai LIKE %s OR 
+                    ruangan LIKE %s OR 
+                    bed LIKE %s OR 
+                    tipe LIKE %s OR 
+                    created_by LIKE %s OR 
+                    updated_by LIKE %s
+                )
+            """, [f"%{search_query}%", f"%{search_query}%", f"%{search_query}%", f"%{search_query}%",
+                  f"%{search_query}%", f"%{search_query}%", f"%{search_query}%", f"%{search_query}%"])
+            total_items = cursor.fetchone()[0]
+
+            # Query dengan pagination
+            cursor.execute("""
+                SELECT id, id_project_header, lebar_bahan, lantai, ruangan, bed, tipe 
+                    stik, elevasi, tinggi_vitrase, tinggi_lipatan, nilai_pembagi, created_by, created_date, updated_by, updated_date, is_deleted
+                FROM tb_project_detil
+                WHERE is_deleted = 0 AND (
+                    id_project_header LIKE %s OR 
+                    lebar_bahan LIKE %s OR  
+                    lantai LIKE %s OR 
+                    ruangan LIKE %s OR 
+                    bed LIKE %s OR 
+                    tipe LIKE %s OR 
+                    created_by LIKE %s OR 
+                    updated_by LIKE %s
+                )
+                LIMIT %s OFFSET %s
+            """, [f"%{search_query}%", f"%{search_query}%", f"%{search_query}%",
+                  f"%{search_query}%", f"%{search_query}%", f"%{search_query}%",
+                  f"%{search_query}%", f"%{search_query}%", 
+                  page_size, (page - 1) * page_size])
+
+            rows = cursor.fetchall()
+
+        data = [
+            {
+                "id": row[0],
+                "id_project_header": row[1], 
+                "lebar_bahan": row[2], 
+                "lantai": row[3], 
+                "ruangan": row[4], 
+                "bed": row[5], 
+                "tipe": row[6], 
+                "created_by": row[7],
+                "created_date": row[8],
+                "updated_by": row[9],
+                "updated_date": row[10],
+                "is_deleted": row[11],
+            }
+            for row in rows
+        ]
+
+        return Response({
+            "count": total_items,
+            "total_pages": (total_items // page_size) + (1 if total_items % page_size else 0),
+            "current_page": page,
+            "results": data
+        }, status=status.HTTP_200_OK)
