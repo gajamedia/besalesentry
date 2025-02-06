2ed3b51c7a4a7bc743d6c97f302b17e0fa7d35dd
gajamedia <72379650+gajamedia@users.noreply.github.com>
2025-02-06 21:03:44 +0700

    add project detil (list API, create API)

--- transaksi/project/views.py
+++ transaksi/project/views.py
@@ -234,3 +234,121 @@ class ProjectHeaderViewSet(viewsets.ViewSet):
             "current_page": page,
             "results": data
         }, status=status.HTTP_200_OK)
+
+
+# Detail Project 
+class ProjectDetilViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]  # API hanya bisa diakses oleh user yang login
+
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        responses={200: "Success"}
+    )
+    def list(self, request):
+        """ Mendapatkan semua data tb_project_detail yang belum dihapus """
+        with connections['mysql'].cursor() as cursor:
+            cursor.execute("SELECT id, id_project_header, lebar_bahan, lantai, ruangan, bed, tipe, " + 
+                           "uk_room_l, uk_room_p, uk_room_t, stik, elevasi, tinggi_vitrase, tinggi_lipatan, nilai_pembagi, " +
+                           "created_by, crated_date, updated_by, updated_date, is_deleted " + 
+                           "FROM tb_project_detil WHERE is_deleted = 0")
+            rows = cursor.fetchall()
+
+        data = [
+            {
+                "id": row[0],
+                "id_project_header": row[1],
+                "lebar_bahan": row[2],
+                "lantai": row[3],
+                "ruangan": row[4],
+                "bed": row[5],
+                "tipe": row[6],
+                "uk_room_l": row[7],
+                "uk_room_p": row[8],
+                "uk_room_t": row[9],
+                "stik": row[10],
+                "elevasi": row[11],
+                "tinggi_vitrase": row[12],
+                "tinggi_lipatan": row[11],
+                "nilai_pembagi": row[12],
+                "created_by": row[13],
+                "created_date": row[14],
+                "updated_by": row[15],
+                "updated_date": row[16],
+                "is_deleted": row[17],
+            }
+            for row in rows
+        ]
+        return Response(data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'id_project_header': openapi.Schema(type=openapi.TYPE_STRING, description='id Project header'),
+                'lebar_bahan': openapi.Schema(type=openapi.TYPE_STRING, description='Lebar Bahan'),
+                'lantai': openapi.Schema(type=openapi.TYPE_STRING, description='Lantai'),
+                'ruangan': openapi.Schema(type=openapi.TYPE_STRING, description='Ruangan'),
+                'bed': openapi.Schema(type=openapi.TYPE_STRING, description='No.Bed'),
+                'tipe': openapi.Schema(type=openapi.TYPE_STRING, description='Tipe'),
+                'uk_room_l': openapi.Schema(type=openapi.TYPE_STRING, description='Lebar Ruangan'),
+                'uk_room_p': openapi.Schema(type=openapi.TYPE_STRING, description='Panjang Ruangan'),
+                'uk_room_t': openapi.Schema(type=openapi.TYPE_STRING, description='Tinggi Ruangan'),
+                'stik': openapi.Schema(type=openapi.TYPE_STRING, description='Stik'),
+                'elevasi': openapi.Schema(type=openapi.TYPE_STRING, description='Elevasi'),
+                'tinggi_vitrase': openapi.Schema(type=openapi.TYPE_STRING, description='Tinggi Vitrase'),
+                'tinggi_lipatan': openapi.Schema(type=openapi.TYPE_STRING, description='Tinggi Lipatan'),
+                'nilai_pembagi': openapi.Schema(type=openapi.TYPE_STRING, description='Nilai Pembagi'),
+            },
+            required=['id_project_header']
+        ),
+        responses={201: "Created"}
+    )
+    def create(self, request):
+        """ Create new project detil """
+        # Ambil data dari body request
+        id_project_header = request.data.get("id_project_header")
+        lebar_bahan = request.data.get("lebar_bahan")
+        lantai = request.data.get("lantai", "")
+        ruangan = request.data.get("ruangan")
+        bed = request.data.get("bed", "")
+        tipe = request.data.get("tipe", "")
+        uk_room_l = request.data.get("uk_room_l")
+        uk_room_p = request.data.get("uk_room_p")
+        uk_room_t = request.data.get("uk_room_t")
+        stik = request.data.get("stik", "")
+        elevasi = request.data.get("elevasi")
+        tinggi_vitrase = request.data.get("tinggi_vitrase", "")
+        tinggi_lipatan = request.data.get("tinggi_lipatan", "")
+        nilai_pembagi = request.data.get("nilai_pembagi")
+        username = request.user.username  # Username pengguna yang login
+        created_date = now()
+
+        # Validasi input
+        if not (id_project_header):
+            return Response({"error": "Required fields are missing"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            with connections["mysql"].cursor() as cursor:
+                # Masukkan data ke tabel
+                cursor.execute("""
+                    INSERT INTO tb_project_detil 
+                    (id_project_header, lebar_bahan, lantai, ruangan, bed, tipe, uk_room_l,
+                    uk_room_p, uk_room_t, stik, elevasi, tinggi_vitrase, tinggi_lipatan, nilai_pembagi,            
+                    created_by, created_date, updated_by, updated_date, is_deleted)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 0)
+                """, [
+                    id_project_header, lebar_bahan, lantai, ruangan, bed, tipe,
+                    uk_room_l, uk_room_p, uk_room_t, stik, elevasi, tinggi_vitrase,
+                    tinggi_lipatan, nilai_pembagi, username, created_date, username, created_date
+                ])
+
+            return Response({"message": "Project created successfully"}, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
