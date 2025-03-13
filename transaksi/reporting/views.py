b3cb5c5f919f25d2ed4d094ca95d525e09a3246e
gajamedia <72379650+gajamedia@users.noreply.github.com>
2025-03-14 03:48:59 +0700

    Update API penawaran_summary fix status

--- transaksi/reporting/views.py
+++ transaksi/reporting/views.py
@@ -22,116 +22,122 @@ class PenawaranViewSet(viewsets.ViewSet):
         """ API untuk menampilkan data penawaran berdasarkan ID Project """
         id_project_header = request.GET.get("id_project_header")
 
-        if not id_project_header:
-            return Response({"error": "id_project_heaader is required"}, status=status.HTTP_400_BAD_REQUEST)
+        # if not id_project_header:
+        #     return Response({"error": "id_project_heaader is required"}, status=status.HTTP_400_BAD_REQUEST)
         
         with connections["mysql"].cursor() as cursor:
             # Ambil data project header
-            cursor.execute("""
-                SELECT ph.id, ph.no_project, ph.tgl_project, ph.ket_project, ph.nama_customer, ph.status_project, ph.addr_customer
-                FROM tb_project_header ph
-                WHERE ph.id = %s
-            """, [id_project_header])
-            project = cursor.fetchone()
-
-            if not project:
-                return Response({"error": "Project tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
-
-            project_data = {
-                "id": project[0],
-                "no_project": project[1],
-                "tgl_project": project[2],
-                "ket_project": project[3],
-                "nama_customer": project[4],
-                "status_project": project[5],
-                "addr_customer": project[6],
-            }
-
-            # Ambil data project detil
-            cursor.execute("""
-                SELECT id, lebar_bahan, lantai, ruangan, bed, tipe, 
-                       uk_room_l, uk_room_p, uk_room_t, stik, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
-                FROM tb_project_detil
-                WHERE id_project_header = %s
-            """, [id_project_header])
-            project_details = cursor.fetchall()
-
-            if not project_details:
-                return Response({"error": "Detail project tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
-
-            details_data = []
-            qty_map = {}
-
-            for detail in project_details:
-                id_detail = detail[0]
-                lantai = detail[2]
-                ruangan = detail[3]
-                tinggi_lipatan = detail[13]
-
-                key = f"{lantai}_{ruangan}"
-                qty_map[key] = qty_map.get(key, 0) + 1
-
-                tinggi_gorden = detail[8] - detail[10]
-                lebar_gorden = detail[6] + detail[7]
-                tinggi_kain = tinggi_gorden - detail[11]
-                panel = lebar_gorden / detail[12]
-
-                # Ambil item terkait
+            if id_project_header:
                 cursor.execute("""
-                    SELECT bi.item_code, bi.ukuran, bi.harga_beli, bi.harga_jual, jb.nama_jenis
-                    FROM tb_project_detil_item pdi
-                    JOIN tb_bahan bi ON pdi.item_id = bi.id
-                    JOIN tb_jenisbahan jb ON bi.id_jenis = jb.id
-                    WHERE pdi.id_project_detil = %s
-                """, [id_detail])
-                items = cursor.fetchall()
+                    SELECT id, no_project, tgl_project, ket_project, nama_customer, status_project, addr_customer
+                    FROM tb_project_header
+                    WHERE id = %s
+                """, [id_project_header])
+            else:
+                cursor.execute("""
+                    SELECT id, no_project, tgl_project, ket_project, nama_customer, status_project, addr_customer
+                    FROM tb_project_header
+                    WHERE status_project='0'
+                """)
+            projects = cursor.fetchall()
 
-                item_data = []
-                for item in items:
-                    volume = ((tinggi_gorden + tinggi_lipatan) * panel) / 100
-                    item_data.append({
-                        "item_code": item[0],
-                        "ukuran": item[1],
-                        "harga_beli": item[2],
-                        "harga_jual": item[3],
-                        "jenis_bahan": item[4],
-                        "volume": volume
+            if not projects:
+                return Response({"error": "Tidak ada proyek yang ditemukan"}, status=status.HTTP_404_NOT_FOUND)
+
+            response_data = []
+            
+            for project in projects:
+                project_data = {
+                    "id": project[0],
+                    "no_project": project[1],
+                    "tgl_project": project[2],
+                    "ket_project": project[3],
+                    "nama_customer": project[4],
+                    "status_project": project[5],
+                    "addr_customer": project[6],
+                }
+
+                # Ambil data project detil
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, bed, tipe, 
+                        uk_room_l, uk_room_p, uk_room_t, stik, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                    WHERE id_project_header = %s
+                """, [project_data["id"]])
+                project_details = cursor.fetchall()
+
+                details_data = []
+                qty_map = {}
+
+                for detail in project_details:
+                    id_detail = detail[0]
+                    lantai = detail[2]
+                    ruangan = detail[3]
+                    tinggi_lipatan = detail[13]
+
+                    key = f"{lantai}_{ruangan}"
+                    qty_map[key] = qty_map.get(key, 0) + 1
+
+                    tinggi_gorden = detail[8] - detail[10]
+                    lebar_gorden = detail[6] + detail[7]
+                    tinggi_kain = tinggi_gorden - detail[11]
+                    panel = lebar_gorden / detail[12]
+
+                    # Ambil item terkait
+                    cursor.execute("""
+                        SELECT bi.item_code, bi.ukuran, bi.harga_beli, bi.harga_jual, jb.nama_jenis
+                        FROM tb_project_detil_item pdi
+                        JOIN tb_bahan bi ON pdi.item_id = bi.id
+                        JOIN tb_jenisbahan jb ON bi.id_jenis = jb.id
+                        WHERE pdi.id_project_detil = %s
+                    """, [id_detail])
+                    items = cursor.fetchall()
+
+                    item_data = []
+                    for item in items:
+                        volume = ((tinggi_gorden + tinggi_lipatan) * panel) / 100
+                        item_data.append({
+                            "item_code": item[0],
+                            "ukuran": item[1],
+                            "harga_beli": item[2],
+                            "harga_jual": item[3],
+                            "jenis_bahan": item[4],
+                            "volume": volume
+                        })
+
+                    details_data.append({
+                        "lantai": lantai,
+                        "ruangan": ruangan,
+                        "bed": detail[4],
+                        "tipe": detail[5],
+                        "tinggi_gorden": tinggi_gorden,
+                        "lebar_gorden": lebar_gorden,
+                        "tinggi_kain": tinggi_kain,
+                        "panel": panel,
+                        "items": item_data
                     })
 
-                details_data.append({
-                    "lantai": lantai,
-                    "ruangan": ruangan,
-                    "bed": detail[4],
-                    "tipe": detail[5],
-                    "tinggi_gorden": tinggi_gorden,
-                    "lebar_gorden": lebar_gorden,
-                    "tinggi_kain": tinggi_kain,
-                    "panel": panel,
-                    "items": item_data
+                # Menambahkan Qty berdasarkan lantai dan ruangan yang sama
+                for detail in details_data:
+                    key = f"{detail['lantai']}_{detail['ruangan']}"
+                    detail["qty"] = qty_map[key]
+                    for item in detail["items"]:
+                        item["volume"] *= detail["qty"]  # Sesuaikan volume berdasarkan Qty
+
+                response_data.append({
+                    "customer": {
+                        "nama_customer": project_data["nama_customer"],
+                        "addr_customer": project_data["addr_customer"]
+                    },
+                    "project": {
+                        "no_project": project_data["no_project"],
+                        "tgl_project": project_data["tgl_project"],
+                        "ket_project": project_data["ket_project"]
+                    },
+                    "details": details_data
                 })
 
-        # Menambahkan Qty berdasarkan lantai dan ruangan yang sama
-        for detail in details_data:
-            key = f"{detail['lantai']}_{detail['ruangan']}"
-            detail["qty"] = qty_map[key]
-            for item in detail["items"]:
-                item["volume"] *= detail["qty"]  # Sesuaikan volume berdasarkan Qty
-
-        response_data = {
-            "customer": {
-                "nama_customer": project_data["nama_customer"],
-                "addr_customer": project_data["addr_customer"]
-            },
-            "project": {
-                "no_project": project_data["no_project"],
-                "tgl_project": project_data["tgl_project"],
-                "ket_project": project_data["ket_project"]
-            },
-            "details": details_data
-        }
-
         return Response(response_data, status=status.HTTP_200_OK)    
-    
     #@action(detail=False, methods=["get"], url_path="penawaran_summary")
     # def penawaran_summary(self, request):
     #     """
