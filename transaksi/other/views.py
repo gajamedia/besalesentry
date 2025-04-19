5d7413a5d9a10292aedf673c7d51e734f68c81f0
gajamedia <72379650+gajamedia@users.noreply.github.com>
2025-04-19 23:38:25 +0700

    Add API kebutuhan kain dan kebutuhan vitrase

--- transaksi/other/views.py
+++ transaksi/other/views.py
@@ -79,3 +79,64 @@ class OtherViewSet(viewsets.ViewSet):
             "total_kebutuhan_kain": round(kebutuhan_total, 2),
             "details": detail_list
         }, status=status.HTTP_200_OK)
+    
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('id_project_detil', openapi.IN_QUERY, description="ID Project Detail (Opsional)", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer ")
+        ],
+        responses={200: "Success"}
+    )
+    def kebutuhan_vitrase(self, request):
+        """ Menghitung total kebutuhan vitrase untuk semua detil project """
+        id_project_detil = request.GET.get("id_project_detil")
+
+        with connections["mysql"].cursor() as cursor:
+            if id_project_detil:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                    WHERE id = %s
+                """, [id_project_detil])
+            else:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                """)
+
+            detils = cursor.fetchall()
+
+            if not detils:
+                return Response({"error": "Data detil tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
+
+            kebutuhan_total = 0
+            detail_list = []
+
+            for detil in detils:
+                uk_room_l = detil[4]
+                uk_room_p = detil[5]
+                tinggi_vitrase = detil[8]
+                nilai_pembagi = detil[9]
+                tinggi_lipatan = detil[10]
+
+                lebar_total = uk_room_l + uk_room_p
+                panel = lebar_total / nilai_pembagi
+
+                kebutuhan = ((tinggi_vitrase + tinggi_lipatan) * panel) / 100
+                kebutuhan_total += kebutuhan
+
+                detail_list.append({
+                    "tinggi_vitrase": tinggi_vitrase,
+                    "tinggi_lipatan": tinggi_lipatan,
+                    "lebar_total": lebar_total,
+                    "panel": panel,
+                    "kebutuhan_vitrase": kebutuhan
+                })
+
+        return Response({
+            "total_kebutuhan_vitrase": round(kebutuhan_total, 2),
+            "details": detail_list
+        }, status=status.HTTP_200_OK)
