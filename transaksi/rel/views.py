0e9d20573c548d82a53af2bbdf85621c57efd04a
gajamedia <72379650+gajamedia@users.noreply.github.com>
2025-08-14 19:54:29 +0700

    update tambahan api
    1. bracket ceiling
    2. fisher
    3. galvanis24
    4. jasa pasang

--- transaksi/rel/views.py
+++ transaksi/rel/views.py
@@ -360,3 +360,269 @@ class RelViewSet(viewsets.ViewSet):
             "total_qty_stik": round(kebutuhan_qty_stik, 2),
             "harga_stik": round(harga_total_stik, 2)
         }, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer "),
+            openapi.Parameter('id_project_detil', openapi.IN_QUERY, description="ID Project Detail (Opsional)", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('item_code', openapi.IN_QUERY, description="Kode Item", type=openapi.TYPE_STRING)
+        ],
+        responses={200: "Success"}
+    )
+    def kebutuhan_bracket_ceiling(self, request):
+        """ Menghitung total kebutuhan Bracket L untuk semua detil project """
+        id_project_detil = request.GET.get("id_project_detil")
+        item_code = request.GET.get("item_code")
+
+        with connections["mysql"].cursor() as cursor:
+            if id_project_detil:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                    WHERE id = %s
+                """, [id_project_detil])
+            else:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                """)
+
+            detils = cursor.fetchall()
+
+            if not detils:
+                return Response({"error": "Data detil tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
+
+            kebutuhan_qty_bracket_ceiling = 0
+
+            for detil in detils:
+                uk_room_l = detil[4]
+                uk_room_p = detil[5]
+                # nilai_pembagi = detil[9]
+
+                lebar_total = uk_room_l + uk_room_p
+                # panel = math.ceil(lebar_total / nilai_pembagi)
+                panel = round((lebar_total / 60),0) + 4
+
+                # kebutuhan_qty_rel += kebutuhan_qty_rel
+                kebutuhan_qty_bracket_ceiling += panel
+
+        # Ambil harga jual dari tb_bahan
+        harga_satuan = 0
+        if item_code:
+            with connections["mysql"].cursor() as cursor:
+                cursor.execute("""
+                    SELECT harga_jual 
+                    FROM tb_bahan
+                    WHERE item_code = %s
+                    LIMIT 1
+                """, [item_code])
+                row = cursor.fetchone()
+                if row:
+                    harga_satuan = row[0]
+
+        # Hitung harga total rel
+        harga_total_bracket_ceiling = kebutuhan_qty_bracket_ceiling * harga_satuan
+
+        return Response({
+            "total_qty_bc": round(kebutuhan_qty_bracket_ceiling, 2),
+            "harga_bc": round(harga_total_bracket_ceiling, 2)
+        }, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer "),
+            openapi.Parameter('id_project_detil', openapi.IN_QUERY, description="ID Project Detail (Opsional)", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('item_code', openapi.IN_QUERY, description="Kode Item", type=openapi.TYPE_STRING)
+        ],
+        responses={200: "Success"}
+    )
+    def kebutuhan_fisher(self, request):
+        """ Menghitung total kebutuhan Bracket L untuk semua detil project """
+        id_project_detil = request.GET.get("id_project_detil")
+        item_code = request.GET.get("item_code")
+
+        with connections["mysql"].cursor() as cursor:
+            if id_project_detil:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                    WHERE id = %s
+                """, [id_project_detil])
+            else:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                """)
+
+            detils = cursor.fetchall()
+
+            if not detils:
+                return Response({"error": "Data detil tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
+
+            kebutuhan_qty_fisher = 0
+
+            for detil in detils:
+                uk_room_l = detil[4]
+                uk_room_p = detil[5]
+                # nilai_pembagi = detil[9]
+
+                lebar_total = uk_room_l + uk_room_p
+                # panel = math.ceil(lebar_total / nilai_pembagi)
+                panel = round((lebar_total / 60),0) + 4
+
+                # kebutuhan_qty_rel += kebutuhan_qty_rel
+                kebutuhan_qty_fisher += panel
+
+        # Ambil harga jual dari tb_bahan
+        harga_satuan = 0
+        if item_code:
+            with connections["mysql"].cursor() as cursor:
+                cursor.execute("""
+                    SELECT harga_jual 
+                    FROM tb_bahan
+                    WHERE item_code = %s
+                    LIMIT 1
+                """, [item_code])
+                row = cursor.fetchone()
+                if row:
+                    harga_satuan = row[0]
+
+        # Hitung harga total rel
+        harga_total_fisher = kebutuhan_qty_fisher * harga_satuan
+
+        return Response({
+            "total_qty_fisher": round(kebutuhan_qty_fisher, 2),
+            "harga_fisher": round(harga_total_fisher, 2)
+        }, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer "),
+            openapi.Parameter('id_project_detil', openapi.IN_QUERY, description="ID Project Detail (Opsional)", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('item_code', openapi.IN_QUERY, description="Kode Item", type=openapi.TYPE_STRING)
+        ],
+        responses={200: "Success"}
+    )
+    def kebutuhan_galvanis24(self, request):
+        """ Menghitung total kebutuhan rel untuk semua detil project """
+        id_project_detil = request.GET.get("id_project_detil")
+        item_code = request.GET.get("item_code")
+
+        with connections["mysql"].cursor() as cursor:
+            if id_project_detil:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                    WHERE id = %s
+                """, [id_project_detil])
+            else:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                """)
+
+            detils = cursor.fetchall()
+
+            if not detils:
+                return Response({"error": "Data detil tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
+
+            kebutuhan_qty_galvanis24 = 0
+
+            for detil in detils:
+                uk_room_l = detil[4]
+                uk_room_p = detil[5]
+
+                # kebutuhan_qty_rel += kebutuhan_qty_rel
+                kebutuhan_qty_galvanis24 += (uk_room_l + uk_room_p) / 100
+
+        # Ambil harga jual dari tb_bahan
+        harga_satuan = 0
+        if item_code:
+            with connections["mysql"].cursor() as cursor:
+                cursor.execute("""
+                    SELECT harga_jual 
+                    FROM tb_bahan
+                    WHERE item_code = %s
+                    LIMIT 1
+                """, [item_code])
+                row = cursor.fetchone()
+                if row:
+                    harga_satuan = row[0]
+
+        # Hitung harga total rel
+        harga_total_galvanis24 = kebutuhan_qty_galvanis24 * harga_satuan
+
+        return Response({
+            "total_qty_galvanis24": round(kebutuhan_qty_galvanis24, 2),
+            "harga_galvanis24": round(harga_total_galvanis24, 2)
+        }, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description="Token JWT", type=openapi.TYPE_STRING, default="Bearer "),
+            openapi.Parameter('id_project_detil', openapi.IN_QUERY, description="ID Project Detail (Opsional)", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('item_code', openapi.IN_QUERY, description="Kode Item", type=openapi.TYPE_STRING)
+        ],
+        responses={200: "Success"}
+    )
+    def jasa_pasang(self, request):
+        """ Menghitung total kebutuhan rel untuk semua detil project """
+        id_project_detil = request.GET.get("id_project_detil")
+        item_code = request.GET.get("item_code")
+
+        with connections["mysql"].cursor() as cursor:
+            if id_project_detil:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                    WHERE id = %s
+                """, [id_project_detil])
+            else:
+                cursor.execute("""
+                    SELECT id, lebar_bahan, lantai, ruangan, 
+                        uk_room_l, uk_room_p, uk_room_t, elevasi, tinggi_vitrase, nilai_pembagi, tinggi_lipatan
+                    FROM tb_project_detil
+                """)
+
+            detils = cursor.fetchall()
+
+            if not detils:
+                return Response({"error": "Data detil tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
+
+            jasa_pasang_qty = 0
+
+            for detil in detils:
+                uk_room_l = detil[4]
+                uk_room_p = detil[5]
+
+                # kebutuhan_qty_rel += kebutuhan_qty_rel
+                jasa_pasang_qty += (uk_room_l + uk_room_p) / 100
+
+        # Ambil harga jual dari tb_bahan
+        harga_satuan = 0
+        if item_code:
+            with connections["mysql"].cursor() as cursor:
+                cursor.execute("""
+                    SELECT harga_jual 
+                    FROM tb_bahan
+                    WHERE item_code = %s
+                    LIMIT 1
+                """, [item_code])
+                row = cursor.fetchone()
+                if row:
+                    harga_satuan = row[0]
+
+        # Hitung harga total rel
+        harga_total_jasa_pasang = jasa_pasang_qty * harga_satuan
+
+        return Response({
+            "total_qty_jasapasang": round(jasa_pasang_qty, 2),
+            "harga_jasapasang": round(harga_total_jasa_pasang, 2)
+        }, status=status.HTTP_200_OK)
